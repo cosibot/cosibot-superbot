e1f6f82b90275f381fb551c951dea3a9174a2af8
pedrosousa-twoimpulse <pedro.sousa@twoimpulse.com>
2020-04-15 17:46:11 +0100

    dummy start intent defined: to emulate button start type "abc" or "start".
    the SetSlotValue action not working as it should, as it does not set the entity properly... maybe a pipeline issue, the extractor not working? Could be the nlu intent definition?

--- actions.py
+++ actions.py
@@ -7,12 +7,16 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+
+import logging
+logger = logging.getLogger(__name__)
+
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action #, Tracker
+from rasa_sdk import Action#, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
-from rasa_core.events import SlotSet
+from rasa_sdk.events import SlotSet
 
 class AskLanguage(Action):
 
@@ -29,13 +33,20 @@ class SetSlotValue(Action):
      def name(self):
          return "action_init_slot"
 
-     def run(self, dispatcher,tracker,domain):
+     def run(self, dispatcher, tracker, domain):
+         lang_slot = tracker.current_state()
+         #lang_slot = next(tracker.get_latest_entity_values("language"), None)
+         dispatcher.utter_message(text=str(lang_slot))
+         logger.debug("Stop point!")
+         return [UserUtteranceReverted()]
 
-         slot_value = tracker.get_slot("language_slot")
-         entity_value = tracker.get_latest_entity_values("language")
+        #  slot_value = tracker.get_slot("language_slot")
+        #  entity_value = tracker.get_latest_entity_values("language")
 
-         if slot_value is None and entity_value is not None:
-             return [SlotSet("language_slot", entity_value)]
-         else:
-             return [SlotSet("language_slot", "en")]
+        #  if slot_value is None and entity_value is not None:
+        #      dispatcher.utter_message(text = "type a" + tracker.get_slot("language_slot"))
+        #      return [SlotSet("language_slot", entity_value)]
+        #  else:
+        #      dispatcher.utter_message(text = "type b" + tracker.get_slot("language_slot"))
+        #      return [SlotSet("language_slot", "en")]
         
